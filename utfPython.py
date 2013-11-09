24c9d1efbbec1c66265058d171f87decd94fc7a0
Rolf Fredheim <fredheir@tcd.ie>
2013-11-09 09:32:43 +0000

    Create utfPython.py

---
+++ utfPython.py
@@ -0,0 +1,18 @@
+
+import codecs
+def file_contents(file_name):
+    with codecs.open(file_name,encoding="utf-8") as f:
+        try:
+            return f.read()
+        finally:
+            f.close()
+
+output=u'something more interesting here'
+writer=open("my_out_file.txt",'w+') # w+ means overwrite - use with care
+    writer.write(output.encode("UTF-8"))
+writer.close()
+
+exampleLookup <- function(path){
+	text=system(paste0("python exampleLookup.py ",path"),intern=T)
+	return (text)
+}
